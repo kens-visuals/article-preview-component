398da16a4b372269daf4254fe2ba877552b4039d
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2021-09-30 13:03:24 -0700

    add JS functionalityfor mobile and desktop screens

--- script.js
+++ script.js
@@ -1,22 +1,12 @@
 `use strict`;
 
 const shareBtn = document.querySelectorAll('.js-share-icon');
-const animateContainer = document.querySelector('.js-animatie-container');
 const shareContainer = document.querySelector('.card__info-container--active');
 
 shareBtn.forEach((el) =>
   el.addEventListener('click', () => {
-    const shareContainerStyles = getComputedStyle(shareContainer);
-
-    if (
-      shareContainerStyles.display === 'none' &&
-      !shareContainer.classList.contains('js-animatie-container')
-    ) {
-      shareContainer.style.display = 'flex';
-      shareContainer.classList.add('js-animatie-container');
-    } else {
-      shareContainer.style.display = 'none';
-      shareContainer.classList.remove('js-animatie-container');
-    }
+    shareContainer.classList.toggle('card__info-container--active');
+    shareContainer.classList.toggle('js-animatie-container--fadeIn');
+    el.classList.toggle('js-share-icon--active');
   })
 );
